7bd73e7d7eece7065887e12d82d608a96de0920d
kstoyanov <kstoyanovv@abv.bg>
2025-04-14 21:39:14 +0300

    that was actually correct

--- v1-http1/server.js
+++ v1-http1/server.js
@@ -35,7 +35,7 @@ server.on('error', (error) => console.error(`[ERROR]: ${error.message}`));
 server.on('close', () => console.log('[CLOSE]: Server is closing the connection..'));
 server.on('connection', (socket) => console.log(`[CONNECTION]: ${socket.remoteAddress} connected`));
 server.on('clientError', (error, socket) => console.error(`[CLIENT ERROR]: ${socket.remoteAddress} errored out with: ${error.message}`));
-server.listen(PORT, () => console.log(`Server listening on http://0.0.0.0:${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
 
 function writeResponse(res, data, contentType) {
     res.writeHead(res.statusCode, { 'Content-Type': contentType });
